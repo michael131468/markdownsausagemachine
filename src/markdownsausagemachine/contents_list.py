71520c1cf9cde4ce6e3b5a0958b2dbbbcacf61f1
Michael Ho <micho@redhat.com>
2025-02-24 13:08:26 +0100

    contents_list.py: share implementations of lists
    Provide a shared abstract class between UnorderedList and OrderedList so
    they can share logic we can handle both types with a trivial
    isinstance() call.

--- src/markdownsausagemachine/contents_list.py
+++ src/markdownsausagemachine/contents_list.py
@@ -1,19 +1,33 @@
+from abc import abstractmethod
 from collections.abc import Collection
 
 from markdownsausagemachine.contents_paragraph import Paragraph
 from markdownsausagemachine.document import SectionContent
 
+type ListItem = str | Paragraph
 
-class UnorderedList(SectionContent):
-    def __init__(self, items: Collection[str]) -> None:
+
+class MarkdownList(SectionContent):
+    """Shared abstraction for list classes"""
+
+    def __init__(self, items: Collection[ListItem]) -> None:
         self.items = items
+        self.nesting_level = 0
+
+    @abstractmethod
+    def get_initial_indent(self, item_no: int) -> str: ...
 
     def get_markdown(self) -> str:
         markdown = ""
         for i, item in enumerate(self.items):
+            initial_indent = self.get_initial_indent(i)
             if isinstance(item, str):
                 item = Paragraph(item)
-                item.initial_indent = f"*   "
+                item.initial_indent = initial_indent
+                item.subsequent_indent = f"{' '*len(item.initial_indent)}"
+                markdown += item.get_markdown()
+            elif isinstance(item, Paragraph):
+                item.initial_indent = initial_indent
                 item.subsequent_indent = f"{' '*len(item.initial_indent)}"
                 markdown += item.get_markdown()
             else:
@@ -24,21 +38,11 @@ class UnorderedList(SectionContent):
         return markdown
 
 
-class OrderedList(SectionContent):
-    def __init__(self, items: Collection[str]) -> None:
-        self.items = items
+class UnorderedList(MarkdownList):
+    def get_initial_indent(self, item_no: int) -> str:
+        return f"{' '*(self.nesting_level*4)}*   "
 
-    def get_markdown(self) -> str:
-        markdown = ""
-        for i, item in enumerate(self.items):
-            if isinstance(item, str):
-                item = Paragraph(item)
-                item.initial_indent = f"{i+1}.  "
-                item.subsequent_indent = f"{' '*len(item.initial_indent)}"
-                markdown += item.get_markdown()
-            else:
-                raise ValueError(f"Unsupported list item type: {type(item)}")
-            # Add some separation between items
-            if i != len(self.items) - 1:
-                markdown += "\n"
-        return markdown
+
+class OrderedList(MarkdownList):
+    def get_initial_indent(self, item_no: int) -> str:
+        return f"{' '*(self.nesting_level*4)}{item_no+1}.  "
